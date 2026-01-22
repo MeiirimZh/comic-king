5dba136b1e8ef18608e0f36df838593a15346785
MeiirimZh <zh.meyirim@gmail.com>
2026-01-22 10:28:28 +0500

    added texts and image in Home

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Главное', style: TextStyle(fontSize: 30))
+    return Scaffold(
+      appBar: AppBar(title: const Text('Главное')),
+      body: Center(child: Column(spacing: 20.0, children: [
+        Column(children: [
+          Text('Персонаж дня', style: TextStyle(fontSize: 20, color: Color(0xFF4D4D4D))),
+          Text('Синий Жук', style: TextStyle(fontSize: 32)),
+        ],),
+        Image.asset('assets/images/BlueBeetle.png', width: 240, height: 240, fit: BoxFit.contain),
+        Text('Lorem ipsum dolor sit amet')
+      ],))
     );
   }
 }
